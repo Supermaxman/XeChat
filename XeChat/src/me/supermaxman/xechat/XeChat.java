002fcbd8cad23ad4b336dbf01b15e9b559f1968c
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2012-07-21 05:16:05 +0100

    small underping

--- XeChat/src/me/supermaxman/xechat/XeChat.java
+++ XeChat/src/me/supermaxman/xechat/XeChat.java
@@ -11,53 +11,56 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 import java.util.List;
 
-public class XeChat extends JavaPlugin implements Listener{
+public class XeChat extends JavaPlugin implements Listener {
 
-	//Required
-	public static XeChat plugin;
-	public static FileConfiguration conf;
-	public XeChatListener Listener = new XeChatListener(this);
+    //Required
+    public static XeChat plugin;
+    public static FileConfiguration conf;
+    public XeChatListener Listener = new XeChatListener(this);
     public XeChatCommandExecutor CommandExecutor = new XeChatCommandExecutor(this);
-	public static Permission permission = null;
-	public static HashMap<Player, String> channelIn = new HashMap<Player, String>();
-	public static HashMap<Player, List<String>> channelsOn = new HashMap<Player, List<String>>();
-	
-     //herpus
+    public static Permission permission = null;
+    public static HashMap<Player, String> channelIn = new HashMap<Player, String>();
+    public static HashMap<Player, List<String>> channelsOn = new HashMap<Player, List<String>>();
 
-	@Override
-	public void onDisable() {System.out.println("[XeChat]: Disabled.");}
-	@Override
-	public void onEnable() {
-		setupPermissions();
+    //herpus
+
+    @Override
+    public void onDisable() {
+        System.out.println("[XeChat]: Disabled.");
+    }
+
+    @Override
+    public void onEnable() {
+        setupPermissions();
         getServer().getPluginManager().registerEvents(Listener, this);
         conf = getConfig();
-		PluginDescriptionFile pdfFile = this.getDescription();
-		System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
-		XeChat.plugin = this;
-		setupConfig();
-		saveConfig();
-		
-		
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+        this.plugin = this;
+        setupConfig();
+
+
         getCommand("l").setExecutor(CommandExecutor);
         getCommand("g").setExecutor(CommandExecutor);
         //getCommand("c").setExecutor(CommandExecutor);
         //getCommand("d").setExecutor(CommandExecutor);
-	}
-	
-    public void setupConfig(){
-    	if (conf.get("worldinchat") == null) {
+    }
+
+    public void setupConfig() {
+        if (conf.get("worldinchat") == null) {
             conf.set("worldinchat", false);
         }
-    	if (conf.get("defaultChannel") == null) {
+        if (conf.get("defaultChannel") == null) {
             conf.set("defaultChannel", "G");
         }
-    	if (conf.get("localdistence") == null) {
+        if (conf.get("localdistence") == null) {
             conf.set("localdistence", 100);
         }
+        saveConfig();
     }
-    
-    
-    private boolean setupPermissions(){
+
+
+    private boolean setupPermissions() {
         RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
         if (permissionProvider != null) {
             permission = permissionProvider.getProvider();
