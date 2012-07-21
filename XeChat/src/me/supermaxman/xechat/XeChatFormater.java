df443281860d780ab057aa63398c448ae849eea3
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2012-07-21 06:07:17 +0100

    simplified chat formatting code a little

--- XeChat/src/me/supermaxman/xechat/XeChatFormater.java
+++ XeChat/src/me/supermaxman/xechat/XeChatFormater.java
@@ -6,24 +6,11 @@ import org.bukkit.entity.Player;
 public class XeChatFormater {
 
     public static String format(Player p, String group, String m, String name, String world, String ch, ChatColor dcolor) {
-        boolean worldchatbool = XeChat.conf.getBoolean("worldinchat");
 
-        String worldchat = "";
-
-        if (worldchatbool) {
-            worldchat = ChatColor.WHITE + "[" + world + "]";
-        }
-
-
-//        if (p.isOp()) {
-//            color = ChatColor.DARK_RED;
-//        } else if (group.equalsIgnoreCase("admin")) {
-//            color = ChatColor.GOLD;
-//        }
         name = dcolor + XeChat.chat.getPlayerPrefix(p).replaceAll("&", "§") + name + dcolor + ":";
         ch = dcolor + "[" + dcolor + ch + dcolor + "]";
-        if (worldchatbool) {
-            return (String.format("%s %s %s %s", ch, worldchat, name, m));
+        if (XeChat.conf.getBoolean("worldinchat")) {
+            return (String.format("%s %s %s %s", ch, ChatColor.WHITE + "[" + world + "]", name, m));
 
         } else {
             return (String.format("%s %s %s", ch, name, m));
