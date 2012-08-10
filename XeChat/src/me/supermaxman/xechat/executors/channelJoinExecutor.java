6abe51b9c4d0d753f107b1077f8a2ac8b681a484
Rainbow <tehrainbowguy@gmail.com>
2012-08-10 22:46:35 +0100

    Equals ignore case

--- XeChat/src/me/supermaxman/xechat/executors/channelJoinExecutor.java
+++ XeChat/src/me/supermaxman/xechat/executors/channelJoinExecutor.java
@@ -5,6 +5,8 @@ import me.supermaxman.xechat.XeChat;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
+
 public class channelJoinExecutor extends baseExecutor {
     @Override
     protected void run(Player player, String[] args) {
@@ -12,40 +14,49 @@ public class channelJoinExecutor extends baseExecutor {
             String channelName = args[0];
 
 
-            if (XeChat.channels.containsKey(channelName)) {
+            if (args.length == 1) {
                 XeChat.isWhispering.put(player, false);
-                XeChannel channel = XeChat.channels.get(channelName);
-                if (!channel.getPlayers().contains(player.getName())) {
-                    if (channel.isPrivate()) {
-                        if (args.length > 1) {
-                            String pass = args[1];
-                            if (pass.equalsIgnoreCase(channel.getPassword())) {
-                                channel.addPlayer(player);
-                                XeChat.channelIn.put(player, channel);
+                XeChannel channel = null;
+                for (Map.Entry<String, XeChannel> channelEntry : XeChat.channels.entrySet()) {
+                    if(channelName.equalsIgnoreCase(channelEntry.getKey())){
+                        channel = channelEntry.getValue();
+                    }
+                }
 
-                                player.sendMessage(ChatColor.AQUA + "[XeChat]: Now Talking In " + channel.getColor() + channel.getName() + ChatColor.AQUA + ".");
+//                XeChannel channel = XeChat.channels.get(channelName);
+                if (channel != null) {
+                    if (!channel.getPlayers().contains(player.getName())) {
+                        if (channel.isPrivate()) {
+                            if (args.length > 1) {
+                                String pass = args[1];
+                                if (pass.equalsIgnoreCase(channel.getPassword())) {
+                                    channel.addPlayer(player);
+                                    XeChat.channelIn.put(player, channel);
+
+                                    player.sendMessage(ChatColor.AQUA + "[XeChat]: Now Talking In " + channel.getColor() + channel.getName() + ChatColor.AQUA + ".");
+                                } else {
+                                    player.sendMessage(ChatColor.RED + "[XeChat]: Incorrect Password For  " + channelName + ".");
+                                }
                             } else {
-                                player.sendMessage(ChatColor.RED + "[XeChat]: Incorrect Password For  " + channelName + ".");
+                                player.sendMessage(ChatColor.RED + "[XeChat]: The Channel " + channelName + " Requires A Password.");
                             }
                         } else {
-                            player.sendMessage(ChatColor.RED + "[XeChat]: The Channel " + channelName + " Requires A Password.");
+                            channel.addPlayer(player);
+                            XeChat.channelIn.put(player, channel);
+
+                            player.sendMessage(ChatColor.AQUA + "[XeChat]: Now Talking In " + channel.getColor() + channel.getName() + ChatColor.AQUA + ".");
                         }
                     } else {
-                        channel.addPlayer(player);
                         XeChat.channelIn.put(player, channel);
 
                         player.sendMessage(ChatColor.AQUA + "[XeChat]: Now Talking In " + channel.getColor() + channel.getName() + ChatColor.AQUA + ".");
                     }
-                } else {
-                    XeChat.channelIn.put(player, channel);
-
-                    player.sendMessage(ChatColor.AQUA + "[XeChat]: Now Talking In " + channel.getColor() + channel.getName() + ChatColor.AQUA + ".");
                 }
             } else {
                 player.sendMessage(ChatColor.RED + "[XeChat]: The Channel " + channelName + " Does Not Exist. Type /create " + channelName + " To  Create It.");
             }
 
-        } else if (args.length == 0) {
+        } else if (args.length != 1) {
 
             player.sendMessage(ChatColor.RED + "[XeChat]: SYNTAX ERROR, type /join [ChannelName].");
 
